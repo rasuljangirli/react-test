83b7d22fcb377684bad98d7a8a72766522ef5d0d
Rasul Jangirli <resulcahangirli99@gmail.com>
2024-05-15 17:20:07 +0400

    first

---
+++ one/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>Lorem ipsum dolor sit amet.</div>
+    </div>
+  );
+}
+
+export default App;
